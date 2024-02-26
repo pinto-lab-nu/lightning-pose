a52741be1248ddab4fc4fd0edfa6879e258b1353
cameycalzolano <114766484+cameycalzolano@users.noreply.github.com>
2024-02-26 14:55:44 -0600

    Update operantScope_DLC_LP_conversion.sh

--- scripts/operantScope_DLC_LP_conversion.sh
+++ scripts/operantScope_DLC_LP_conversion.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=0-00:30:00 
+#SBATCH --time=0-01:30:00 
 #SBATCH --job-name=operantScope_DLC_LP_conversion
 #SBATCH --mem=50GB 
 #SBATCH --ntasks=1
 
 
 
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pose_estimation
 python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/converters/dlc2lp.py --dlc_dir="/mnt/fsmresfiles/Operant_Data/videos/DeepLabCut/projects/OperantScope-CC-2024-02-19" --lp_dir="/home/nfh2911/Documents/Experiments_Operant/LP_projects"
 
