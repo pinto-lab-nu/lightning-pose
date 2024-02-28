010e31ee5e19d3362e48399fbcdf371c71b548fb
cameycalzolano <114766484+cameycalzolano@users.noreply.github.com>
2024-02-28 13:03:22 -0600

    Create trainingLPModel_OperantScope.sh

---
+++ scripts/trainingLPModel_OperantScope.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=0-01:30:00 
+#SBATCH --job-name=trainingLPModel_OperantScope
+#SBATCH --mem=50GB 
+#SBATCH --ntasks=1
+
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate pose_estimation
+python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/train_hydra.py --config-path="/home/nfh2911/Documents/Experiments_Operant/OperantScope_LP" --config-name="config_default.yaml"
+
