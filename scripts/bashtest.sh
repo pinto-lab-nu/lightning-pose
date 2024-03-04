1857931f9ae245e43d863c7ae52fe59b1abef92e
cameycalzolano <114766484+cameycalzolano@users.noreply.github.com>
2024-03-04 13:04:23 -0600

    Create bashtest.sh

---
+++ scripts/bashtest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=0-00:01:00 
+#SBATCH --job-name=trainingLPModel_OperantScope
+#SBATCH --mem=2GB 
+#SBATCH --ntasks=1
+#SBATCH --nodelist=FSM2JSX7Y3
+
+
+echo "hello"
