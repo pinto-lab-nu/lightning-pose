86155c113aebca6464f63b694450d5e0c254b2f2
cameycalzolano <114766484+cameycalzolano@users.noreply.github.com>
2024-03-04 10:00:03 -0600

    update for new conversion with cropped frames

--- scripts/operantScope_DLC_LP_conversion.sh
+++ scripts/operantScope_DLC_LP_conversion.sh
@@ -9,5 +9,5 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pose_estimation
-python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/converters/dlc2lp.py --dlc_dir="/mnt/fsmresfiles/Operant_Data/videos/DeepLabCut/projects/OperantScope-CC-2024-02-19" --lp_dir="/home/nfh2911/Documents/Experiments_Operant/LP_projects"
+python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/converters/dlc2lp.py --dlc_dir="/mnt/fsmresfiles/Operant_Data/videos/DeepLabCut/projects/OperantScope-CC-2024-02-19_toLP" --lp_dir="/home/nfh2911/Documents/Experiments_Operant/OperantScope_LP"
 
