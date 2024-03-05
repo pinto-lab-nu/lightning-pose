6c4de3b6757228b3e4b65d888dcaf0622c6ed5ff
cameycalzolano <114766484+cameycalzolano@users.noreply.github.com>
2024-03-05 16:39:44 -0600

    Update operant tracking scripts

--- scripts/track_new_videos.sh
+++ scripts/track_new_videos.sh
@@ -13,6 +13,6 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate pose_estimation 
 
-python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/convert_behav_video.py concat_behav_video "$1/$2"
+python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/convert_behav_video.py concat_behav_video "$1"
 
-python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/predict_new_vids.py --config-path="/home/nfh2911/Documents/Experiments_Operant/OperantScope_LP" --config-name="config_default.yaml" eval.hydra_paths=["2024-03-04/12-57-49"] eval.test_videos_directory="$1/$2" eval.saved_vid_preds_dir="$1/$2" eval.save_vids_after_training=true
+python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/predict_new_vids.py --config-path="/home/nfh2911/Documents/Experiments_Operant/OperantScope_LP" --config-name="config_default.yaml" eval.hydra_paths=["2024-03-04/12-57-49"] eval.test_videos_directory="$1" eval.saved_vid_preds_dir="$1" eval.save_vids_after_training=true
--- scripts/track_new_videos_batch.sh
+++ scripts/track_new_videos_batch.sh
@@ -2,13 +2,13 @@
 
 basename = "mnt/fsmresfiles/Operant_Data/imaging/DMS_imaging"
 
-recs="IM7/2024_02_09/15_05_50/My_WebCam
+recs="mnt/fsmresfiles/Operant_Data/imaging/DMS_imaging/IM7/2024_02_09/15_05_50/My_WebCam
 "
 
 for recname in $recs
 do
     echo $recname
-	sbatch /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/track_new_videos.sh $basename $recname
+	sbatch /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/track_new_videos.sh $recname
 	sleep 1
 done
 
