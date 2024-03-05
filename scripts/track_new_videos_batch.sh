9cf2a630754f6d3aac2d794e54002327640670fc
cameycalzolano <114766484+cameycalzolano@users.noreply.github.com>
2024-03-05 16:20:44 -0600

    Update track_new_videos_batch.sh
    add full pathname of track new videos back

--- scripts/track_new_videos_batch.sh
+++ scripts/track_new_videos_batch.sh
@@ -8,7 +8,7 @@ recs="IM7/2024_02_09/15_05_50/My_WebCam
 for recname in $recs
 do
     echo $recname
-	sbatch track_new_videos.sh $basename $recname
+	sbatch /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/track_new_videos.sh $basename $recname
 	sleep 1
 done
 
