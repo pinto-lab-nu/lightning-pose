6cf67f86285ffe5acba0fef38f680016c8cef20e
cameycalzolano <114766484+cameycalzolano@users.noreply.github.com>
2024-03-06 13:03:21 -0600

    Update track_new_videos.sh

--- scripts/track_new_videos.sh
+++ scripts/track_new_videos.sh
@@ -9,16 +9,15 @@
 #SBATCH --mail-user=julia.cox@northwestern.edu
 #SBATCH -o /mnt/fsmresfiles/Operant_Data/videos/lightning_pose/logs/%N.%j.out       # STDOUT
 #SBATCH -e /mnt/fsmresfiles/Operant_Data/videos/lightning_pose/logs/%N.%j.err       # STDERR
-#SBATCH --array=57%10
+#SBATCH --array=0%10
 
-config=/mnt/fsmresfiles/Operant_Data/videos/lightning_pose/videosToTrack.txt
-IFS=$'\n' read -d '' -r -a fname < $config
+fname=($(</mnt/fsmresfiles/Operant_Data/videos/lightning_pose/videosToTrack.txt))
 
 
 source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate pose_estimation 
 
-python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/convert_behav_video.py "$fname[$SLURM_ARRAY_TASK_ID]"
+python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/convert_behav_video.py "${fname[$SLURM_ARRAY_TASK_ID]}"
 
-python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/predict_new_vids.py --config-path="/home/nfh2911/Documents/Experiments_Operant/OperantScope_LP" --config-name="config_default.yaml" eval.hydra_paths=["/mnt/fsmresfiles/Operant_Data/videos/lightning_pose/outputs/2024-03-04/12-57-49"] eval.test_videos_directory="$fname[$SLURM_ARRAY_TASK_ID]" eval.saved_vid_preds_dir="$fname[$SLURM_ARRAY_TASK_ID]" eval.save_vids_after_training=false
+python /home/nfh2911/Documents/Experiments_Operant/lightning-pose/scripts/predict_new_vids.py --config-path="/home/nfh2911/Documents/Experiments_Operant/OperantScope_LP" --config-name="config_default.yaml" eval.hydra_paths=["/mnt/fsmresfiles/Operant_Data/videos/lightning_pose/outputs/2024-03-04/12-57-49"] eval.test_videos_directory="${fname[$SLURM_ARRAY_TASK_ID]}" eval.saved_vid_preds_dir="${fname[$SLURM_ARRAY_TASK_ID]}" eval.save_vids_after_training=false
